27229de2fd47dd0c26327280293f40424500c4cf
siva kumar <sivakumar.k@nuacem.com>
2024-09-04 00:37:55 +0530

    modified:   src/routes/gitRoutes.js

--- src/routes/gitRoutes.js
+++ src/routes/gitRoutes.js
@@ -5,7 +5,7 @@ const gitWebhookController = require("../controllers/gitWebhookController");
 // POST route for Git webhook
 router.post("/webhook", gitWebhookController.handleWebhook);
 router.get("/test", (req, res) => {
-  res.status(200).json({ message: "Test successful" });
+  res.status(200).json({ message: "Test successfull" });
 });
 router.get("/refresh", gitWebhookController.refresh);
 module.exports = router;
